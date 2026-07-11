faea7162973c7a78ecd27ae6c795f3a4a45d77a4
windows <cesar.bautista@uelectronics.com>
2026-07-10 19:02:07 -0600

    style: improve UI

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -1,46 +1,46 @@
 import {
   useMediaQuery
-} from "./chunk-5D5OIOGU.js";
+} from "./chunk-E55KZHSE.js";
 import {
   computed,
   ref,
   shallowRef,
   watch
-} from "./chunk-3ZOBRW44.js";
+} from "./chunk-VKQ3VEVA.js";
 
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
-import "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import { default as default3 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
-import { default as default4 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default5 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPFeatures.vue";
-import { default as default6 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
-import { default as default7 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default8 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default9 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default10 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
-import { default as default11 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPLink.vue";
-import { default as default12 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPNavBarSearch.vue";
-import { default as default13 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPSocialLink.vue";
-import { default as default14 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPSocialLinks.vue";
-import { default as default15 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
-import { default as default16 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
-import { default as default17 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default18 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default19 } from "/home/jonathan/Documentos/Docs/github-mx/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
+import "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import { default as default3 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
+import { default as default4 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default5 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPFeatures.vue";
+import { default as default6 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
+import { default as default7 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default8 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default9 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default10 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
+import { default as default11 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPLink.vue";
+import { default as default12 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPNavBarSearch.vue";
+import { default as default13 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPSocialLink.vue";
+import { default as default14 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPSocialLinks.vue";
+import { default as default15 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
+import { default as default16 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import { default as default17 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default18 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default19 } from "C:/Users/Isaac/Documents/labs/unit_devlab_rp_examples/docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
 
 // node_modules/vitepress/dist/client/theme-default/composables/local-nav.js
 import { onContentUpdated } from "vitepress";
--- docs/index.md
+++ docs/index.md
@@ -1,62 +1,12 @@
 ---
-layout: home
-
-hero:
-  name: RP2040 Labs
-  text: Ejemplos y Prácticas para RP2040
-  tagline: Documentación práctica para programar el microcontrolador RP2040 en ensamblador y C, con ejemplos de comunicación UART, I²C, SPI, ADC y más.
-  actions:
-    - theme: brand
-      text: Ver ejemplos
-      link: /examples/
-    - theme: alt
-      text: Guía de inicio
-      link: /guide/devlab
-
-features:
-  - title: Arquitectura Cortex-M0+
-    details: Implementación práctica utilizando RP2040 como plataforma compatible con arquitectura ARM Cortex-M0+.
-  - title: Ensamblador y C
-    details: Ejemplos en lenguaje ensamblador y C para entender la programación de bajo nivel en microcontroladores.
-  - title: Periféricos completos
-    details: Prácticas con GPIO, UART, I²C, SPI, ADC y temporizadores del RP2040.
-  - title: Herramientas de desarrollo
-    details: Flujo de desarrollo moderno con SDK oficial y herramientas open-source.
+layout: page
+title: RP2040 Labs
+sidebar: false
+aside: false
 ---
 
-## Contenido Temático
+<script setup>
+import Home from './.vitepress/components/Home.vue'
+</script>
 
-- **Arquitecturas de microcontroladores** de propósito general
-- Implementación utilizando **RP2040** como plataforma compatible con arquitectura Cortex-M0+
-- Programación en **lenguaje ensamblador**
-- Programación en **lenguaje C**
-- Programación en **Arduino IDE**
-- **Entradas y salidas digitales**
-- **Comunicación UART**
-- **Comunicación I²C**
-- **Comunicación SPI**
-- **ADC y temporizadores**
-- Herramientas de desarrollo para microcontroladores
-
-## Prácticas de Laboratorio
-
-- [Lectura de entradas digitales](./examples/gpio.md)
-- [Comunicación UART](./examples/uart.md)
-- [Lectura de temperatura mediante ADC](./examples/adc.md)
-- [Comunicación I²C con display OLED](./examples/i2c_oled.md)
-
-## Guías de Desarrollo
-
-- [Configuración del SDK](./guide/devlab.md)
-- [Guía para Windows](./guide/windows.md)
-- [Programación en C](./guide/c-programming.md)
-- [Programación en Ensamblador](./guide/asm-programming.md)
-
-## Uso Básico
-
-```bash
-npm install
-npm run docs:dev
-```
-
-El sitio se sirve localmente con VitePress y documenta los ejemplos que viven en este repositorio.
+<Home />
