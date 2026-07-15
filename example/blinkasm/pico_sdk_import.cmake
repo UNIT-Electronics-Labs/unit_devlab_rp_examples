1e84722c033ea6e247cec99c006acddddf130609
obviousfancy <jmejoradol1800@alumno.ipn.mx>
2026-07-14 18:59:44 -0600

    fix: legacy example codes deleted

--- example/blinkasm/pico_sdk_import.cmake
+++
@@ -1,5 +0,0 @@
-if(NOT DEFINED ENV{PICO_SDK_PATH})
-    message(FATAL_ERROR "PICO_SDK_PATH is not set. Run the build with 'picodev build'.")
-endif()
-
-include($ENV{PICO_SDK_PATH}/external/pico_sdk_import.cmake)
